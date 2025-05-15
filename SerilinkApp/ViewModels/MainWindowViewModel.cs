bdd91db24947651d3ab255f58e115401be60afe0
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2025-05-15 11:42:10 +0800

    :lipstick: implement main window UI

--- SerilinkApp/ViewModels/MainWindowViewModel.cs
+++ SerilinkApp/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,26 @@
-﻿namespace SerilinkApp.ViewModels;
+﻿using System.Collections.Generic;
+
+namespace SerilinkApp.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    /// <summary>
+    /// The available baud rates for the serial port.
+    /// </summary>
+    public IEnumerable<string> BaudRates { get; } = new[]
+    {
+        "300", "600", "1200", "2400", "4800", "9600", "14400",
+        "19200", "28800", "38400", "57600", "115200"
+    };
+
+    /// <summary>
+    /// Default baud rate for the serial port.
+    /// </summary>
+    private string _selectedBaudRate = "9600";
+
+    public string SelectedBaudRate
+    {
+        get => _selectedBaudRate;
+        set => SetProperty(ref _selectedBaudRate, value);
+    }
 }
