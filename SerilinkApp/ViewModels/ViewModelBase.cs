84092699eaa61c3044fb5092e5444b61e087b0c6
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2025-05-15 08:40:16 +0800

    :tada: Initial commit
    Created with Avalonia template MVVM with .NET 8

---
+++ SerilinkApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SerilinkApp.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ SerilinkApp/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SerilinkApp.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
